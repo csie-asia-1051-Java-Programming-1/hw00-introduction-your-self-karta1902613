6a3c5b44bebf9ff59d2bfe284214816d17fb5820
karta1902613 <105021043@live.asia.edu.tw>
2016-09-12 15:47:20 +0800

    hw000
    My information

---
+++ hw000/src/hw000/hw000.java
@@ -0,0 +1,7 @@
+package hw000;
+  public class hw000 {
+	public static void main(String[] args) {
+	  // TODO Auto-generated method stub
+		System.out.println("姓名：劉凱文");
+		System.out.println("學號：105021043");
+		System.out.println("科系：CSIE");}}
